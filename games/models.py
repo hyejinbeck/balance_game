387615ebae57fe3e0e51ce6af55db798c05c5193
hyejin beck <beckhye@gmail.com>
2023-09-01 15:06:40 +0900

    기본셋팅완료

---
+++ games/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Game(models.Model): 
+    title = models.CharField(max_length=50) #제목
+    question = models.TextField()   # 질문
+    answer1 = models.TextField()
+    answer2 = models.TextField()
+
+class Answer(models.Model): 
+    game = models.ForeignKey(Game,on_delete=models.CASCADE)
+    choice = models.TextField()
